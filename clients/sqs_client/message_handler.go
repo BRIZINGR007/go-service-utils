9e2aa1c4e0db759d30ef953b97a42a4915f727ce
Nabarka Deb <nabarka.deb@u-next.com>
2025-04-12 16:01:52 +0530

    Added some changes to the service  utils  ...

--- clients/sqs_client/message_handler.go
+++ clients/sqs_client/message_handler.go
@@ -25,60 +25,6 @@ func GetEventProcessor() *EventProcessor {
 	return instance
 }
 
-// func (p *EventProcessor) HandleEvents(messageBody structs.MessageBody) error {
-// 	handler, exists := p.EventHandlers[messageBody.Event]
-// 	if !exists {
-// 		log.Printf("no handler registered for event : %s", messageBody.Event)
-// 		return nil
-// 	}
-// 	err := handler(messageBody.Payload)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return nil
-// 	// handlerValue := reflect.ValueOf(handler)
-// 	// handlerType := handlerValue.Type()
-
-// 	// if handlerType.Kind() != reflect.Func || handlerType.NumIn() != 1 {
-// 	// 	log.Printf("invalid handler signature for event: %s", messageBody.Event)
-// 	// 	return nil
-// 	// }
-
-// 	// // Get the type of the function's first parameter
-// 	// argType := handlerType.In(0)
-
-// 	// // Convert map[string]string payload to JSON
-// 	// payloadBytes, err := json.Marshal(messageBody.Payload)
-// 	// if err != nil {
-// 	// 	log.Printf("failed to marshal payload: %v", err)
-// 	// 	return nil
-// 	// }
-
-// 	// // Create a new instance of the argument type and unmarshal into it .
-// 	// // If we fail to verify the  payload then we delete the message from the  queue  .
-// 	// argValue := reflect.New(argType).Interface()
-// 	// if err := json.Unmarshal(payloadBytes, argValue); err != nil {
-// 	// 	log.Printf("failed to unmarshal payload into handler argument: %v", err)
-// 	// 	return nil
-// 	// }
-
-// 	// // Call the handler with the argument (dereference pointer if needed)
-// 	// reflectArg := reflect.ValueOf(argValue)
-// 	// if reflectArg.Kind() == reflect.Ptr {
-// 	// 	reflectArg = reflectArg.Elem()
-// 	// }
-
-// 	// returnValues := handlerValue.Call([]reflect.Value{reflectArg})
-
-// 	// if len(returnValues) > 0 && !returnValues[0].IsNil() {
-// 	// 	if returnValues[0].Type().Implements(reflect.TypeOf((*error)(nil)).Elem()) {
-// 	// 		return returnValues[0].Interface().(error)
-// 	// 	}
-// 	// }
-// 	return nil
-
-// }
-
 func (p *EventProcessor) HandleEvents(messageBody structs.MessageBody) error {
 	handler, exists := p.EventHandlers[messageBody.Event]
 	if !exists {
