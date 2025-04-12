0045121bc2d317167682c807b0bc3e89a512b8af
Nabarka Deb <nabarka.deb@u-next.com>
2025-04-12 19:03:41 +0530

    Added  chnages  to  the FIFO Queue Posting  Message  Logic  ...

--- clients/sqs_client/post_message.go
+++ clients/sqs_client/post_message.go
@@ -3,6 +3,7 @@ package sqs_client
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/BRIZINGR007/go-service-utils/structs"
@@ -10,25 +11,24 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
-func PostMessageNonFIFO(queueURL string, payload *structs.MessageBody) {
+func PostMessageNonFIFO(queueURL string, payload *structs.MessageBody) error {
 	client, err := InitSQSClient()
 	if err != nil {
-		log.Fatalf("Failed to initialize SQS client: %v", err)
-	}
-	messageBody, err := json.Marshal(payload)
-	if err != nil {
-		log.Fatalf("Failed to marshal message body: %v", err)
+		return fmt.Errorf("failed to initialize SQS client: %w", err)
 	}
 
-	// Send the message
+	messageBody, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message body: %w", err)
+	}
+
 	_, err = client.SendMessage(context.TODO(), &sqs.SendMessageInput{
-		QueueUrl:    &queueURL,
+		QueueUrl:    aws.String(queueURL),
 		MessageBody: aws.String(string(messageBody)),
 	})
 	if err != nil {
-		log.Fatalf("Failed to send message: %v", err)
+		return fmt.Errorf("failed to send message to queue %s: %w", queueURL, err)
 	}
-
 	log.Println("Message sent successfully to queue:", queueURL)
-
+	return nil
 }
